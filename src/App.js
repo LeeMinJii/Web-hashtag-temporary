ff6c3f9ab1e24ddea3c73cb90f756c424d80fcf2
MinjiLee <julia7450@naver.com>
2024-03-17 22:29:07 +0900

    하단부분, 정렬문제해결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import logo from './icon-256x256.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <img className='logo' src={logo}></img>
+      </div>
+
+      <Group/>
+      <CopyButton/>
+      <hr/>
+      <Popular/>
+
     </div>
   );
+}
+
+// 제일 위 인기해시태그 모음 - 한방에 복사하는 것
+function Group(){
+
+  let [GroupHashtags, setGroupHashtags] = useState(['#인기해시태그1', '#인기해시태그2', '#인기해시태그3', '#인기해시태그4', '#인기해시태그5', '#인기해시태그6','#인기해시태그7','#인기해시태그8','#인기해시태그9']);
+
+  return (
+    <div className='inner-background'>
+      <div className='item-container'>
+        {GroupHashtags.map((hashtag, index) => (
+          <div key={index} className='above-item'>
+            {hashtag}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function CopyButton(){
+  return (
+    <div className='CopyBtn-container'>
+      <button className='CopyButton'>복붙하기</button>
+    </div>
+  );
+}
+
+function Popular(){
+
+  let [PopularHashtags, setPopularHashtags] = useState(['#성수동', '#성수동', '#성수동', '#성수동', '#성수동', '#성수동','#성수동','#성수동','#성수동']);
+
+  return (
+    <div>
+      <h2 style={{marginLeft: '5%', textAlign: 'left'}}>⭐ 인기해시태그</h2>
+
+      <div className='inner-background'>
+        <div className='item-container'>
+          {PopularHashtags.map((hashtag, index) => (
+            <div key={index} className='below-item'>
+              {hashtag}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
 }
 
 export default App;
